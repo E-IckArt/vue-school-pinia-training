ad34375bc77b1dd0ef700d7ed1b904c70103cc20
Emilie Ickart <emi.ickart@gmail.com>
2023-03-18 16:35:43 +0100

    Create new getters to count and display items by name in the cart

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,5 @@
 import {defineStore} from "pinia";
+import {groupBy} from "lodash";
 
 export const useCartStore = defineStore("CartStore", {
     state: () => {
@@ -19,6 +20,9 @@ export const useCartStore = defineStore("CartStore", {
     },
     getters: {
         count: (state) => state.items.length,
-        isEmpty: (state) => state.count === 0
-    }
+        isEmpty: (state) => state.count === 0,
+        // Use groupBy method of lodash library to group item by name
+        grouped: (state) => groupBy(state.items, item => item.name),
+        groupCount: (state) => (name) => state.grouped[name].length,
+    },
 });
